5ff5e871a933f21a335eeeb4980795d724124fca
franciscobspadaro <francisco.spadaro@outlook.com>
2020-09-01 20:12:25 -0300

    Versão 3.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,18 @@ import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 
 const IndexPage = () => (
-  <>
   <Layout>
     <SEO title="Home" />
-    <PostItem />
-</Layout>
-</>
+    <PostItem
+      slug="/about/"
+      category="Misc"
+      date="30 de Julho de 2019"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+    />
+  </Layout>
 )
+
 
 export default IndexPage
