1799a0750cb4389934e4fe52991318cd4886f709
franciscobspadaro <francisco.spadaro@outlook.com>
2020-09-02 22:01:04 -0300

    Versão 3.9

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-import Layout from "../src/components/Layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 
