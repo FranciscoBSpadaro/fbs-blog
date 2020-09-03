e4549c4029551703d08f04a5ececf66e735465da
franciscobspadaro <francisco.spadaro@outlook.com>
2020-09-03 17:55:33 -0300

    Versão 4.5

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,9 +2,10 @@ import React from "react"
 import Profile from "../Profile"
 import SocialLinks from "../SocialLinks"
 import MenuLinks from "../MenuLinks"
-
-
-
+import { Link } from "gatsby"
+import catsGIF from "../Sidebar/cats.gif"
+import fbsdevPNG from "../Sidebar/fbsdev.png"
+import devGIF from "../Sidebar/dev.gif"
 
 import * as S from "./styled"
 
@@ -13,6 +14,12 @@ const Sidebar = () => (
     <Profile />
     <SocialLinks />
     <MenuLinks />
+    <Link href="https://fbsdev.netlify.app/">
+    <img src={fbsdevPNG} alt="FBS-DEV" ></img>
+		</Link>
+    <img src={catsGIF} width="" height="" alt="Funcionarios" ></img> 
+    <img src={devGIF} alt="dev" ></img>
+
   </S.SidebarWrapper>
 )
 
