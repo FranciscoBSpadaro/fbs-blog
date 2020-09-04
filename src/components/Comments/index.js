69d1e830eb4eee43f85a3887996e95520eecc128
franciscobspadaro <francisco.spadaro@outlook.com>
2020-09-04 15:20:24 -0300

    Versão 5.0

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -11,7 +11,7 @@ const Comments = ({ url, title }) => {
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="franciscobianchispadaro"
+        shortname="https-fbs-blog-netlify-app"
         identifier={completeURL}
         title={title}
         url={completeURL}
