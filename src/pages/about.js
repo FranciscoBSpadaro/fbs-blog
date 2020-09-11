591cb827f8574ad468d14e89cafeb0b44179f93a
franciscobspadaro <francisco.spadaro@outlook.com>
2020-09-11 14:05:04 -0300

    7.3

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,14 +18,16 @@ const AboutPage = () => (
 <h2><img src={catsGIF} width="250" height="250" alt="funcionarios" ></img></h2>
 <p>Olá muito prazer, me chamo Francisco Bianchi Spadaro.<span role="img" aria-label="handet">🖖🏽</span>
 <br/> Estou cursando Análise e Desenvolvimento de Sistemas pela faculdade Anhanguera e estou nos cursos de Programação oferecidos por  Dev Samurai, Rockeseat, udemy, etc.
-<br/> Meu foco é dominar o JavaScript e com isso dominar o ReactJs, React Native e NodeJs, e tambem atuar como DevOps nas plataformas AWS 'Amazon Web Services '  e IBM CLOUD.
+<br/> Meu foco é dominar o JavaScript e com isso dominar o ReactJs, React Native e tambem atuar como DevOps nas plataformas de Cloud.
 <br/> Busco sempre melhorar como pessoa e profissional e assim melhorar o mundo e as pessoas ao meu redor<span role="img" aria-label="laptop">💻</span> <span role="img" aria-label="hearb">🌎</span> 
 <br/> <span role="img" aria-label="rocket">🚀</span>Atualmente estou desempregado mas planejo atuar como freelancer , programador ou DevOps
 <br/> <span role="img" aria-label="laptop">💻</span> <span role="img" aria-label="hearb">💙</span>  Busco colaborar em projetos tanto no Front-end quanto no Back-end usando ReactJs, React Native e NodeJs. 
 <br/> Posso tentar te ajudar com HTML,CSS, Javascript,  ReactJs, React Native e NodeJs.
 <br/> <span role="img" aria-label="laptop">💻</span> Minhas stacks favoritas: React Native, ReactJs e NodeJs. (JavaScript) Python.
 <br/> <span role="img" aria-label="sound">🎵</span> <span role="img" aria-label="tree">🎄</span>  Atividades: Curto praia , natureza , músicas e filmes/seriados em geral.
-<br/> Mas oque eu mais faço é estudar , estou totalmente engajado e focado nisso 
+<br/> Mas oque eu mais faço é estudar , estou totalmente engajado e focado nisso.
+<br/> Sou questionador e de mente empreendedora , não gosto de comodismo e sempre quero mudar e evoluir para o melhor.
+<br/> Busco me preparar para a terceira revolução industrial.
 <br/> <span role="img" aria-label="ipod">📟</span>  Entre em contato comigo Click no logotipo abaixo</p> 
 <h2><Link to="https://fbsdev.netlify.app/"> <img src={fbsdevPNG} alt="FBS-DEV"></img> </Link></h2>
 </S.MainContent>   
